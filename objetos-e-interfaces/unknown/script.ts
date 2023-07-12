905138b3d05165b0012e724ee1fa883c3f300cfd
Otávio Borges <otavio.demb@hotmail.com>
2023-07-12 11:36:39 -0300

    Unknown

---
+++ objetos-e-interfaces/unknown/script.ts
@@ -0,0 +1,18 @@
+// Indica que não sabemos o tipo de dados que pode ser passado.
+// Diferente do any, o unknown só irá permitir o uso de métodos quando a Type Safety estiver garantida.
+
+function typeGuard(value: unknown) {
+  if (typeof value === 'string') {
+    return value.toLowerCase();
+  }
+  if (typeof value === 'number') {
+    return value.toFixed();
+  }
+  if (value instanceof HTMLElement) {
+    return value.innerText;
+  }
+}
+
+console.log(typeGuard('Origamid'));
+console.log(typeGuard(200));
+console.log(typeGuard(document.body));
