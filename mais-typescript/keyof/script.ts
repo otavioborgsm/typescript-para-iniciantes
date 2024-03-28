1a03632fe1ad642a24573f6b1c5344a40413bbca
Otávio Borges <otavio.demb@hotmail.com>
2024-03-28 00:01:27 -0300

    keyof para validação de objetos

---
+++ mais-typescript/keyof/script.ts
@@ -0,0 +1,97 @@
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+// let chave: "nome" | "produto" - campos de Produto;
+let chave: keyof Produto;
+
+chave = 'nome';
+chave = 'preco';
+
+function coordenadas(x: number, y: number) {
+  return { x, y };
+}
+
+let coord: typeof coordenadas;
+// Deve ter o valor abaixo
+coord = (x: number, y: number) => {
+  return { x, y };
+};
+
+interface Elementos {
+  a: HTMLAnchorElement;
+  video: HTMLVideoElement;
+  div: HTMLElement;
+  body: HTMLBodyElement;
+  audio: HTMLAudioElement;
+}
+
+function selecionar<Chave extends keyof Elementos>(
+  seletor: Chave,
+): null | Elementos[Chave] {
+  return document.querySelector(seletor);
+}
+
+console.log(selecionar('body'))
+
+
+async function fetchData<T>(url: string): Promise<T> {
+  const base = 'https://api.origamid.dev/json';
+  const response = await fetch(base + url);
+  return await response.json();
+}
+
+interface Jogo {
+  nome: string;
+  ano: number;
+  desenvolvedora: string;
+  plataformas: string[];
+}
+
+interface Livro {
+  nome: string;
+  ano: number;
+  autor: string;
+  paginas: number;
+}
+
+async function handleData() {
+  const jogo = await fetchData('/jogo.json');
+  if (checkInterface<Jogo>(jogo, 'desenvolvedora')) {
+    console.log(jogo.desenvolvedora);
+  }
+
+  const livro = await fetchData('/livro.json');
+  if (checkInterface<Livro>(livro, 'autor')) {
+    console.log(livro.autor);
+  }
+}
+
+handleData();
+
+function checkInterface<Interface>(
+  obj: unknown,
+  ...keys: Array<keyof Interface>
+): obj is Interface {
+  if (
+    obj &&
+    typeof obj === 'object' &&
+    keys.filter((key) => key in obj).length === keys.length
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkInterfaceSimples<Interface>(
+  obj: unknown,
+  key: keyof Interface,
+): obj is Interface {
+  if (obj && typeof obj === 'object' && key in obj) {
+    return true;
+  } else {
+    return false;
+  }
+}
